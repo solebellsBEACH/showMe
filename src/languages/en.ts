321d597656bac0c7625a33a648be51016657ff55
Lucas Xavier <lucassxxavier@gmail.com>
2023-04-10 13:31:13 -0300

    feat:adding english icons

--- src/languages/en.ts
+++ src/languages/en.ts
@@ -3,6 +3,7 @@ import { stackAssets } from '../assets/stacks';
 import { ILanguageJSON } from '../interface';
 import { homeAssets } from '../assets/home';
 import { projectAssets } from '../assets/stacks/projects';
+import { HowICanHelpYouIconsEnum } from '../interface/enums';
 
 const {
   adonisjs,
@@ -72,16 +73,19 @@ export const englishJson: ILanguageJSON = {
           header: 'Quality and High Performance',
           text:
             "I've always worked with current technologies, getting 100% involved with the project and the end result. ",
+          icon: HowICanHelpYouIconsEnum.highPerformace
         },
         {
           header: 'Experience and Knowledge',
           text:
             "Throughout my history, I've worked with different segments and developed the necessary know- how to work on the most diverse projects.",
+          icon: HowICanHelpYouIconsEnum.experience
         },
         {
           header: 'Agility in Development',
           text:
             'I work with agile methodologies that rely on embedded processes and tools and the necessary skills.',
+          icon: HowICanHelpYouIconsEnum.speed
         },
       ],
     },
